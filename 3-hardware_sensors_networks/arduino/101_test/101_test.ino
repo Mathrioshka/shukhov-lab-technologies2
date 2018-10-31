7c6f78fddbe7ce87588dc466337f83a2c740cf7e
Vadim Smakhtin <vadim.smakhtin@habidatum.com>
2018-10-31 11:37:43 +0300

    Added serial test example, especially for Arduino 101 users

---
+++ 3-hardware_sensors_networks/arduino/101_test/101_test.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(57600);
+
+  while (!Serial) ;
+}
+
+int i = 0;
+
+void loop() {  
+  Serial.println(i);
+
+  i++;
+
+  if (i > 100) {
+    i = 0;
+  }
+}
